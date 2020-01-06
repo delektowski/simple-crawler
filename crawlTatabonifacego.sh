e103fb78d09cf7215fc53593a4ba47be0b518252
Marcin Delektowski <marcin.delektowski@gmail.com>
2020-01-06 01:37:58 +0100

    Add exact name of fruit

--- crawlTatabonifacego.sh
+++ crawlTatabonifacego.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for i in {1..200}
-do
-   npx cypress run --spec cypress/integration/plantTataBonifacego.spec.js
-   dtplant=$(date '+%d/%m/%Y %H:%M:%S');
-   echo "Posadzono owocki $i razy. Data: $dtplant"
-   sleep 600
-   dtharvest=$(date '+%d/%m/%Y %H:%M:%S');
-   npx cypress run --spec cypress/integration/harvestTatabonifacego.spec.js
-   echo "Zebrano owocki $i razy. Data: $dtharvest"
+owocki=$(cat <cypress/integration/plantTataBonifacego.spec.js | grep "\Plant\b" | cut -c 18-30)
+
+for i in {1..200}; do
+  npx cypress run --spec cypress/integration/plantTataBonifacego.spec.js
+  dtplant=$(date '+%d/%m/%Y %H:%M:%S')
+  echo "Posadzono $owocki $i razy. Data: $dtplant"
+  sleep 600
+  dtharvest=$(date '+%d/%m/%Y %H:%M:%S')
+  npx cypress run --spec cypress/integration/harvestTatabonifacego.spec.js
+  echo "Zebrano $owocki $i razy. Data: $dtharvest"
 done
