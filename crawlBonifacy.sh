29ef79d34a34792f0f994a6d68b506143856aecd
Marcin Delektowski <marcin.delektowski@gmail.com>
2020-01-06 23:47:27 +0100

    Add time for harvesting nagietki

--- crawlBonifacy.sh
+++ crawlBonifacy.sh
@@ -6,7 +6,7 @@ for j in {1..200}; do
   npx cypress run --spec cypress/integration/plantBonifacy.spec.js
   dtplant=$(date '+%d/%m/%Y %H:%M:%S')
   echo "Posadzono $owocki $j razy. Data: $dtplant"
-  sleep 575
+  sleep 20600
   npx cypress run --spec cypress/integration/harvestBonifacy.spec.js
   dtharvest=$(date '+%d/%m/%Y %H:%M:%S')
   echo "Zebrano $owocki $j razy. Data: $dtharvest"
