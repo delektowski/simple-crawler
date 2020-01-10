c06f3ff394aedebc3575103380f7e06c6224ddbe
md <marcin.delektowski@grupa.it>
2020-01-10 18:40:50 +0100

    Change grillparty vege

--- cypress/integration/plantGrillParty.spec.js
+++ cypress/integration/plantGrillParty.spec.js
@@ -9,7 +9,7 @@ describe("GrillParty", () => {
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
         // Plant sałata
-        cy.get('#regal_2').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        cy.get('#regal_12').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
         // Wait and logout
         cy.wait(5000).logout()
