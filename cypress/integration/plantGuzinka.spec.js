46a268b9c874905d05faee8adce54273a7c1e0e2
md <marcin.delektowski@grupa.it>
2020-03-26 00:49:41 +0100

    Before reset

--- cypress/integration/plantGuzinka.spec.js
+++ cypress/integration/plantGuzinka.spec.js
@@ -9,17 +9,9 @@ describe("Guzinka", () => {
       .wait(500);
 
         // Plant rzodkiewki
-    cy.get("#regal_14")
-      .eq(0)
-      .click({ force: true })
-      .wait(500)
-      .plantingOrWatering()
-      .get("#giessen")
-      .click({ force: true })
-      .wait(500)
-      .plantingOrWatering();
+    cy.get('#regal_12').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
     // Wait and logout
-    cy.wait(5000).logout();
+    cy.wait(2240).logout();
   });
 });
