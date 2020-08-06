50278f7b40bc13cafe19d825ade89b71fb07b8ef
Marcin <marcin.delektowski@gmail.com>
2020-08-06 09:53:34 +0200

    Change bot plant

--- cypress/integration/plantGuzinka.spec.js
+++ cypress/integration/plantGuzinka.spec.js
@@ -8,8 +8,8 @@ describe("Guzinka", () => {
       .click({ force: true })
       .wait(500);
 
-        // Plant rzodkiewki
-    cy.get('#regal_12').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        // Plant truskawki
+    cy.get('#regal_3').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
     // Wait and logout
     cy.wait(2240).logout();
--- cypress/integration/plantSympatyczna.spec.js
+++ cypress/integration/plantSympatyczna.spec.js
@@ -8,8 +8,8 @@ describe("Mama sympatyczna", () => {
         // Select planting
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
-         // Plant rzodkiewki
-         cy.get('#regal_14').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+         // Plant marchewki
+         cy.get('#regal_6').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
 
         // Wait and logout
