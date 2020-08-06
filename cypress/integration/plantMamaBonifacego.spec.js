b6682daafc85e8d70fcd622f966ab98d9050f056
Marcin Delektowski <marcin.delektowski@gmail.com>
2020-08-06 23:06:27 +0200

    Start symaptyczna

--- cypress/integration/plantMamaBonifacego.spec.js
+++ cypress/integration/plantMamaBonifacego.spec.js
@@ -8,8 +8,8 @@ describe("Mama bonifacego", () => {
         // Select planting
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
-        // Plant słonecznik
-        cy.get('#regal_48').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        // Plant marchewki
+        cy.get('#regal_6').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
         // Wait and logout
         cy.wait(4000).logout()
