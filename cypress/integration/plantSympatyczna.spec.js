ba9dc964c05a41557c96b3bcb4d212d8ef0fdb43
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-02-01 20:38:24 +0100

    Fix sympatyczna

--- cypress/integration/plantSympatyczna.spec.js
+++ cypress/integration/plantSympatyczna.spec.js
@@ -9,7 +9,7 @@ describe("Mama sympatyczna", () => {
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
         // Plant marchewki
-        cy.get('#regal_6').eq(0).click({force:true}).wait(500).plantingOrWatering().
+        cy.get('#regal_6').eq(0).click({force:true}).wait(500).plantingOrWatering()
 
         // Select watering
 //         cy.get('#giessen').click({force:true}).wait(500).plantingOrWatering();
