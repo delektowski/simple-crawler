94df4604aa6a8d859012ebecff57001bdd981f14
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-02-18 21:45:35 +0100

    Add plantacjat harvest time

--- crawlPlantacjat.sh
+++ crawlPlantacjat.sh
@@ -6,7 +6,7 @@ for i in {1..2000}; do
   npx cypress run --spec cypress/integration/plantPlantacjat.spec.js
   dtplant=$(date '+%d/%m/%Y %H:%M:%S')
   echo "Posadzono $owocki $i razy. Data: $dtplant"
-  sleep 7980
+  sleep 600
   dtharvest=$(date '+%d/%m/%Y %H:%M:%S')
   npx cypress run --spec cypress/integration/harvestPlantacjat.spec.js
   echo "Zebrano $owocki $i razy. Data: $dtharvest"
