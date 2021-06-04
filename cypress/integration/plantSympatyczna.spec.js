c6290e12df01c90a5cc7e3e41f460a5e56536132
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-06-04 18:43:22 +0200

    config

--- cypress/integration/plantSympatyczna.spec.js
+++ cypress/integration/plantSympatyczna.spec.js
@@ -12,7 +12,7 @@ describe("Mama sympatyczna", () => {
         cy.get('#regal_14').eq(0).click({force:true}).wait(500).plantingOrWatering()
 
         // Select watering
-//         cy.get('#giessen').click({force:true}).wait(500).plantingOrWatering();
+         cy.get('#giessen').click({force:true}).wait(500).plantingOrWatering();
 
         // Wait and logout
         cy.wait(4000).logout()
