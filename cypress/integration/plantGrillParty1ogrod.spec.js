505277e450a7b160245fc7c8f21fc200f0e63d99
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-06-29 19:15:05 +0200

    add rose

--- cypress/integration/plantGrillParty1ogrod.spec.js
+++ cypress/integration/plantGrillParty1ogrod.spec.js
@@ -7,8 +7,8 @@ describe("GrillParty", () => {
         // Select planting
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
-        // Plant nagietki
-        cy.get('#regal_49').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        // Plant róże
+        cy.get('#regal_50').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
     
         // Wait and logout
         cy.wait(5000).logout()
