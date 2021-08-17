37bd7461b5f69b8fa0ea6ff704f74a6dfa01289f
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-08-17 23:01:22 +0200

    Planting

--- crawlGrillParty1ogrod.sh
+++ crawlGrillParty1ogrod.sh
@@ -6,7 +6,7 @@ for j in {1..2000}; do
   npx cypress run --spec cypress/integration/plantGrillParty1ogrod.spec.js
   dtplant=$(date '+%d/%m/%Y %H:%M:%S')
   echo "Posadzono $owocki $j razy. Data: $dtplant"
-  sleep 22000
+  sleep 2400
   npx cypress run --spec cypress/integration/harvestGrillParty1ogrod.spec.js
   dtharvest=$(date '+%d/%m/%Y %H:%M:%S')
   echo "Zebrano $owocki $j razy. Data: $dtharvest"
--- cypress/integration/plantGrillParty1ogrod.spec.js
+++ cypress/integration/plantGrillParty1ogrod.spec.js
@@ -7,8 +7,8 @@ describe("GrillParty", () => {
         // Select planting
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
-        // Plant róże
-        cy.get('#regal_50').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        // Plant ogórki
+        cy.get('#regal_12').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
     
         // Wait and logout
         cy.wait(5000).logout()
