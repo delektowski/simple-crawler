be3469c2b5bd2b5022145c817512ab02fd4903f6
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-10-16 20:14:01 +0200

    Changed tata bonifacego

--- cypress/integration/plantBonifacy.spec.js
+++ cypress/integration/plantBonifacy.spec.js
@@ -8,8 +8,8 @@ describe("Bonifacy", () => {
         // Select planting
         cy.get('#anpflanzen').click({force: true}).wait(500);
         
-        // Plant sałata
-                cy.get('#regal_2').eq(0).click({force:true}).wait(500).plantingOrWatering().get('#giessen').click({force:true}).wait(500).plantingOrWatering();
+        // Plant słoneczniki
+             cy.get('#regal_48').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
         // Wait and logout
         cy.wait(5000).logout()
