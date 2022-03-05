7251ad3818f1ff48587dd00ceae718b55b801a7d
Marcin Delektowski <marcin.delektowski.external@atos.net>
2022-03-05 20:17:30 +0100

    Add nagietki alpejski

--- cypress/integration/plantAlpejski.spec.js
+++ cypress/integration/plantAlpejski.spec.js
@@ -9,7 +9,7 @@ describe("Alpejski", () => {
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
         // Plant truskawki
-        cy.get('#regal_3').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        cy.get('#regal_49').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
         // Wait and logout
         cy.wait(5000).logout()
