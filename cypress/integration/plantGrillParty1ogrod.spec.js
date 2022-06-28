e6d266f74a72e3b3cce59cd10661163ecf0eb608
Marcin Delektowski <marcin.delektowski.external@atos.net>
2022-06-28 21:26:33 +0200

    pomidory

--- cypress/integration/plantGrillParty1ogrod.spec.js
+++ cypress/integration/plantGrillParty1ogrod.spec.js
@@ -7,8 +7,8 @@ describe("GrillParty", () => {
         // Select planting
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
-        // Plant rzodkiew
-            cy.get('#regal_14').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        // Plant pomidory
+        cy.get('#regal_5').eq(0).click({force:true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
     
         // Wait and logout
         cy.wait(5000).logout()
--- cypress/integration/plantSniezna.spec.js
+++ cypress/integration/plantSniezna.spec.js
@@ -8,8 +8,8 @@ describe("Snieżna", () => {
         // Select planting
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
-        // Plant truskawki
-        cy.get('#regal_3').eq(0).click({force: true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        // Plant pomidory
+        cy.get('#regal_5').eq(0).click({force:true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
         // Wait and logout
         cy.wait(5000).logout()
