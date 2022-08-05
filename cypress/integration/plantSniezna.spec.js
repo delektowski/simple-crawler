048b05c0fce1b28e2b096b9fd5143354a1094f52
Marcin <delektowski@interia.pl>
2022-08-05 22:05:48 +0200

    add ogorki

--- cypress/integration/plantSniezna.spec.js
+++ cypress/integration/plantSniezna.spec.js
@@ -8,8 +8,8 @@ describe("Snieżna", () => {
         // Select planting
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
-        // Plant pomidory
-        cy.get('#regal_5').eq(0).click({force:true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        // Plant ogórki
+        cy.get('#regal_12').eq(0).click({force:true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
         // Wait and logout
         cy.wait(5000).logout()
