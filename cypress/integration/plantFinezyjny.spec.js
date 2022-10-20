0da72bff014501ac7b2daca21efa6236d7049c79
Marcin Delektowski <marcin.delektowski@gmail.com>
2022-10-21 00:20:29 +0200

    update

--- cypress/integration/plantFinezyjny.spec.js
+++ cypress/integration/plantFinezyjny.spec.js
@@ -9,7 +9,7 @@ describe("Finezyjny", () => {
         cy.get('#anpflanzen').click({force: true}).wait(500);
 
         // Plant pomidory
-        cy.get('#regal_5').eq(0).click({force:true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
+        cy.get('#regal_48').eq(0).click({force:true}).wait(500).plantingOrWatering().get('#giessen').click({force: true}).wait(500).plantingOrWatering();
 
         // Wait and logout
         cy.wait(5000).logout()
