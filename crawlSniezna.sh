07a6256a2844e13dd20a970791295623ab71d0b4
Marcin Delektowski <marcin.delektowski@gmail.com>
2022-11-07 22:10:45 +0100

    update

--- crawlSniezna.sh
+++ crawlSniezna.sh
@@ -6,7 +6,7 @@ for j in {1..2000}; do
   npx cypress run --spec cypress/integration/plantSniezna.spec.js
   dtplant=$(date '+%d/%m/%Y %H:%M:%S')
   echo "Posadzono $owocki $j razy. Data: $dtplant"
-  sleep 2100
+  sleep 8040
   npx cypress run --spec cypress/integration/harvestSniezna.spec.js
   dtharvest=$(date '+%d/%m/%Y %H:%M:%S')
   echo "Zebrano $owocki $j razy. Data: $dtharvest"
